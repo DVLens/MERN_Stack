9881c8b18cb2436e9362c9cf14e5ad5192dc1ac5
DVLens <dvl.bc.ens@gmail.com>
2024-10-21 20:37:02 -0700

    Intento de peticion

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/',(request,response)=>{
+    console.log(request);
+    return response.status(234).send('Welcome to my  MERN STACK');
+});
+
 app.listen(PORT,()=>{
     console.log("test inicio de servidor");
 });
