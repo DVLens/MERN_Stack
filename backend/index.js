464b417cf0edbba60e6cfb52c5ace4f0da7a6050
DVLens <dvl.bc.ens@gmail.com>
2024-10-25 20:43:08 -0700

    Controladores nuevos de GET, PUT, POST, DELETE

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.post('/books', async(request,response) => {
     const newBook = {
         title: request.body.title,
         author: request.body.author,
-    puvblishYear: request.body.publishYear
+        publishYear: request.body.publishYear
     };
     const book = await Book.create(newBook);
 
@@ -36,10 +36,81 @@ app.post('/books', async(request,response) => {
         console.log(error.message);
         response.status(500).send({message: error.message});
     }
-})
+});
+
+app.get('/books',async(request, response) => {
+    try{
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+});
+
+app.get('/books/:id',async(request, response) => {
+    try{
+        const {id} = request.params;        
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+});
 
 app.listen(PORT,()=>{
     console.log("test inicio de servidor");
+});
+
+//Update
+app.put('/books/:id',async(request,response) => {
+    try{
+        if(!request.body ||
+        !request.body.author ||
+        !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear'
+            });
+        }
+
+        const {id} = request.params;
+
+        const result = await Book.findByIdAndUpdate(id,request.body);
+
+        if(!result){
+            return response.status(404).json({message: 'Book not found'});            
+        }
+        return response.status(200).send({message: 'Book updated successfully'});
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }    
+});
+
+app.delete('/books/:id',async(request,response) =>{
+    try{
+        const {id} = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(404).json({ message:'Book not found'});        
+        }
+        return response.status(200).send({message: 'Book deleted succesfully'});
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
 });
 
 mongoose
